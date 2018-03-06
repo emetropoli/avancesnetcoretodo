db8a97f54fe106c1e3d17fb44ebfcf1a61653a52
emetropoli <emetropoli@gmail.com>
2018-03-06 09:44:23 -0500

    Facebook auth added - New Facebook boton

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,11 +1,12 @@
 ﻿// Write your JavaScript code.
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Wire up the Add button to send the new item to the server
     $('#add-item-button').on('click', addItem);
+    $('#faceclick').on('click', loggeoFb);
 
 
-    $('.done-checkbox').on('click', function(e) {
+    $('.done-checkbox').on('click', function (e) {
         markCompleted(e.target);
     });
 
@@ -15,14 +16,28 @@ function addItem() {
     $('#add-item-error').hide();
     var newTitle = $('#add-item-title').val();
 
-    $.post('/Todo/AddItem', { title: newTitle }, function() {
+    $.post('/Todo/AddItem', { title: newTitle }, function () {
         window.location = '/Todo';
     })
-    .fail(function(data) {
-        if (data && data.responseJSON) {
-            var firstError = data.responseJSON[Object.keys(data.responseJSON)[0]];
-            $('#add-item-error').text(firstError);
-            $('#add-item-error').show();
+        .fail(function (data) {
+            if (data && data.responseJSON) {
+                var firstError = data.responseJSON[Object.keys(data.responseJSON)[0]];
+                $('#add-item-error').text(firstError);
+                $('#add-item-error').show();
+            }
+        });
+}
+
+
+function loggeoFb() {
+    FB.login(function(response) {
+        if (response.authResponse) {
+         console.log('Welcome!  Fetching your information.... ');
+         FB.api('/me', function(response) {
+           console.log('Good to see you, ' + response.name + '.');
+         });
+        } else {
+         console.log('User cancelled login or did not fully authorize.');
         }
     });
 }
@@ -30,7 +45,7 @@ function addItem() {
 function markCompleted(checkbox) {
     checkbox.disabled = true;
 
-    $.post('/Todo/MarkDone', { id: checkbox.name }, function() {
+    $.post('/Todo/MarkDone', { id: checkbox.name }, function () {
         var row = checkbox.parentElement.parentElement;
         $(row).addClass('done');
     });
